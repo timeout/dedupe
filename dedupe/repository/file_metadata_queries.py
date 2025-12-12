ff5e164f5b0988d0e2f2df62f3c44f7798d27fc1
Joe Gain <subscriber@gainable.io>
2025-12-12 11:40:18 +1100

    extracts query templates

---
+++ dedupe/repository/file_metadata_queries.py
@@ -0,0 +1,22 @@
+
+CREATE = """
+    INSERT INTO file_metadata (absolute_path, filename, size, quick_hash)
+    values (?, ?, ?, ?)
+"""
+
+READ_ALL = """
+    SELECT *
+    FROM file_metadata
+"""
+
+READ_ALL_DUPLICATES = """
+    SELECT quick_hash
+    FROM file_metadata
+    GROUP BY quick_hash
+    HAVING COUNT(quick_hash) > 1
+"""
+
+DELETE_BY_ID = """
+    DELETE FROM file_metadata
+    WHERE id = ?
+"""
